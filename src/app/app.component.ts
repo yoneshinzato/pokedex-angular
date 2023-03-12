76ade33e6262cb0700f2171ebca9cedfcb3db2c7
yone-shinzato <chibariyo@gmail.com>
2023-03-12 20:28:06 -0300

    setup inicial da applicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    oi
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'pokedex-angular';
